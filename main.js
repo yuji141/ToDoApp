d38380e620b5261e32a1ecf367b04b4d4dc85f49
yuji141 <android>
2025-09-13 10:56:15 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+const input = document.querySelector('#todoINput');
+const addBtn = document.querySelector('#addBtn');
+const todoList = doucument.querySelector('#todoList');
+
+//追加ボタンが押された時
+addBtn.addEventListener('click', () => {
+  const text = input.value.trim();
+  if (text === "") return; //空文字は無視
+  
+  //li要素を作成
+  const li = document.createElement('li');
+  li.textContent = text;
+  
+  //削除ボタンうぃ作成
+  const delBtn = documetn.createElement('button');
+  delBtn.textContent = '削除';
+  delBtn.addEventListener('click', () => {
+    li.remove();
+  });
+  
+  li.appendChild(delBtn);
+  todoList.appendChild(li);
+  
+  //入力欄を空にする
+  input.value = "";
+});
