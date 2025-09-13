9b42e9f63771f2f891670c15d4dabdd0856d85eb
yuji141 <yuji141@users.noreply.github.com>
2025-09-13 14:15:50 +0900

    コード修正

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const input = document.querySelector('#todoINput');
+const input = document.querySelector('#todoInput');
 const addBtn = document.querySelector('#addBtn');
-const todoList = doucument.querySelector('#todoList');
+const todoList = document.querySelector('#todoList');
 
 //追加ボタンが押された時
 addBtn.addEventListener('click', () => {
   const text = input.value.trim();
-  if (text === "") return; //空文字は無視
-  
+  if (text === '') return; //空文字は無視
+
   //li要素を作成
   const li = document.createElement('li');
   li.textContent = text;
-  
+
   //削除ボタンうぃ作成
-  const delBtn = documetn.createElement('button');
+  const delBtn = document.createElement('button');
   delBtn.textContent = '削除';
   delBtn.addEventListener('click', () => {
     li.remove();
   });
-  
+
   li.appendChild(delBtn);
   todoList.appendChild(li);
-  
+
   //入力欄を空にする
-  input.value = "";
+  input.value = '';
 });
