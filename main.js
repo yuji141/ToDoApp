d43287cfda2ddf39bb527860c1aeb128339401ec
yuji141 <yj>
2025-09-20 11:46:08 +0900

    チェックボックス追加

--- main.js
+++ main.js
@@ -3,14 +3,28 @@ const input = document.querySelector('#todoInput');
 const addBtn = document.querySelector('#addBtn');
 const todoList = document.querySelector('#todoList');
 
+//Todoを追加する関数
 function addTodo() {  
     const text = input.value.trim();
     if (text === '') return; //空文字は無視
   
     //li要素を作成
     const li = document.createElement('li');
-    li.textContent = text;
-  
+    
+    //チェックボックスを作成
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        li.styletextDecoration = 'line-through'; //完了：取り消し線
+      } else {
+        li.styletextDecoration = '';//未完了：もとに戻す
+      }
+    });
+    //テキスト
+    const span = document.createElement('span')；
+    span.textContent = text;
+    
     //削除ボタンを作成
     const delBtn = document.createElement('button');
     delBtn.textContent = '削除';
@@ -18,12 +32,16 @@ function addTodo() {
       li.remove();
     });
   
+    //liに要素をまとめる
+    li.appendChild(checkbox);
+    li.appendChild(span);
     li.appendChild(delBtn);
     todoList.appendChild(li);
   
     //入力欄を空にする
     input.value = '';
   }
+  
   //追加ボタンで追加
   addBtn.addEventListener('click', addTodo);
 
